a8d41cf733ebbfe7fe26c9e68894cebfa6fb9a56
Fanyi <ft2582@columbia.edu>
2023-07-11 17:29:42 -0400

    stuck in computer_player

--- practice-for-ch-ruby-memory-long-practice-main/game.rb
+++ practice-for-ch-ruby-memory-long-practice-main/game.rb
@@ -1,10 +1,12 @@
 require_relative "board.rb"
 require_relative "card.rb"
+require_relative "human_player.rb"
 
 class Game
     def initialize(prev_guess = nil)
         @board = Board.new()
         @prev_guess = prev_guess
+        @human_player = HumanPlayer.new()
         
     end
 
@@ -13,34 +15,10 @@ class Game
         while !@board.won?
             system("clear")
             @board.render
-            p 'enter position in row col'
-            pos = gets.chomp.split(' ')
-            pos[0] =pos[0].to_i
-            pos[1] =pos[1].to_i
+            pos = @human_player.prompt
             make_guess(pos)
         end
     end
-
-    # def make_guess(guessed_pos)
-    #     if !@prev_guess
-    #         @board.reveal(guessed_pos)
-    #         @prev_guess = guessed_pos
-    #     else
-    #         if @board.reveal(guessed_pos) != @board.reveal(@prev_guess)
-    #             @board.render
-    #             sleep(2)
-    #             @board[guessed_pos].face= false
-    #             @board[@prev_guess].face = false
-    #             @prev_guess = nil
-    #         else 
-    #             # @board[guessed_pos].face = true
-    #             # @board[@prev_guess].face = true
-    #             # @board.render
-    #             # sleep(2)
-    #             # @prev_guess = nil
-    #         end
-    #     end
-    # end
 
 
 
--- practice-for-ch-ruby-memory-long-practice-main/human_player.rb
+++ practice-for-ch-ruby-memory-long-practice-main/human_player.rb
@@ -1,5 +1,11 @@
 class HumanPlayer
 
-    
+    def prompt
+        p 'enter position in row col'
+        pos = gets.chomp.split(' ')
+        pos[0] =pos[0].to_i
+        pos[1] =pos[1].to_i
+        return pos
+    end
 
 end
